dfb46bb9c4e5f28ed2dacee0e7476b4391b64c42
ARYAN SHARMA <67369076+SPARKILATOR@users.noreply.github.com>
2022-11-12 16:27:15 +0530

    Update chamiponsRO16_draw.py
    Added class reference to make it reusable to other section.
    Also added some performance related stuff.
    Thanks

--- ChampionsLeague/chamiponsRO16_draw.py
+++ ChampionsLeague/chamiponsRO16_draw.py
@@ -8,41 +8,77 @@ import logging
 import json
 import random
 import time
-logging.basicConfig(filename=r'log.txt', filemode='w', format='%(asctime)s.%(msecs)03d %(name)s - %(levelname)s - %(message)s')
+
+logging.basicConfig(filename=r'log.txt', filemode='w', format='%(asctime)s.%(msecs)03d %(levelname)s - %(message)s')
 
 logging.warning('Champions League Round of 16 Draw Starting .....')
 
+
+def draw_time(t1, t2):
+    # Performance check for the logic
+    return t2 - t1
+
+
+class ChampionsLeague:
+    def __init__(self, data, data_group):
+        self.data = data
+        self.data_group = data_group
+        self.draw_list = []
+        self.t = 0
+
+    def log(self):
+        # Logging the POT1 and POT2 teams also the group details of the league.
+        logging.warning("CHAMPIONS LEAGUE POT 1 : %r" % self.data['Pot1'])
+        logging.warning("CHAMPIONS LEAGUE POT 2 : %r" % self.data['Pot2'])
+        logging.warning("GROUP DETAILS  ::: %r" % self.data_group)
+
+    def draw(self):
+        t1 = time.time()
+        # Main logic to predict the teams
+        for club1, country1 in self.data['Pot1'].items():
+            while True:
+                count = 0
+                # Pick any team from the Pot2
+                a = random.choice(list(self.data['Pot2'].items()))
+                # To handle duplication
+                if (club1 in self.draw_list) or (a[0] in self.draw_list):
+                    continue
+                # Applied Conditions like 2 Teams should not be from same national league
+                # and not from the same group
+                if str(country1) != str(a[1]):
+                    for i in range(1, len(self.data_group) + 1):
+                        if (club1 in self.data_group['GROUP_' + str(i)]) and (
+                                a[0] in self.data_group['GROUP_' + str(i)]):
+                            count = 1
+                            break
+                    if count == 0:
+                        self.draw_list.append(club1)
+                        self.draw_list.append(a[0])
+                        break
+        logging.warning("PLEASE WAIT .....")
+        time.sleep(2)
+        logging.warning("\n")
+        logging.warning("----> CHAMPIONS LEAGUE DRAW LOADING .....")
+        time.sleep(2)
+        for i in range(0, len(self.draw_list), 2):
+            logging.warning("DRAW %r --> %r [%r] VS %r [%r]" % (
+                (i + 2) // 2, self.draw_list[i].upper(), self.data['Pot1'][self.draw_list[i]],
+                self.draw_list[i + 1].upper(),
+                self.data['Pot2'][self.draw_list[i + 1]]))
+            time.sleep(2)
+        logging.warning('\n')
+        t2 = time.time()
+        self.t = round(draw_time(t1, t2), 2)
+        logging.warning("This prediction took  %r seconds " % self.t)
+        logging.warning("----> THANK YOU FOR STAYING WITH US, AND WILL MEET SOON TILL THEN ENJOY RESPONSIBLY .....")
+
+
 if __name__ == "__main__":
-    draw_list = []
     # extracting relevant details from the Json.
     with open(r"details.json") as json_file:
-        data = json.load(json_file)
+        data_pot = json.load(json_file)
     with open(r"group.json") as json_file_group:
-        data_group = json.load(json_file_group)
-    # Logging the POT1 and POT2 teams of the league.
-    logging.warning("CHAMPIONS LEAGUE POT 1 : %r" %(data['Pot1']))
-    logging.warning("CHAMPIONS LEAGUE POT 2 : %r" % (data['Pot2']))
-    # Main logic to predict the teams
-    for club1, country1 in data['Pot1'].items():
-        while True:
-            count = 0
-            a = random.choice(list(data['Pot2'].items()))
-            if (club1 in draw_list) or (a[0] in draw_list):
-                continue
-            if str(country1) != str(a[1]):
-                for i in range(1, 9):
-                    if (club1 in data_group['GROUP_' + str(i)]) and (a[0] in data_group['GROUP_' + str(i)]):
-                        count = 1
-                        break
-                if count == 0:
-                    draw_list.append(club1)
-                    draw_list.append(a[0])
-                    break
-    logging.warning("PLEASE WAIT .....")
-    time.sleep(2)
-    logging.warning("\n")
-    logging.warning("----> CHAMPIONS LEAGUE DRAW LOADING .....")
-    time.sleep(2)
-    for i in range(0,len(draw_list),2):
-        logging.warning("DRAW %r --> %r [%r] VS %r [%r]" %((i+2)//2,draw_list[i],data['Pot1'][draw_list[i]],draw_list[i+1],data['Pot2'][draw_list[i+1]]))
-        time.sleep(2)
+        data_group_detail = json.load(json_file_group)
+    championsRO16_draw = ChampionsLeague(data_pot, data_group_detail)
+    # if u want details of the group and pot, then do this championsRO16_draw.log()
+    championsRO16_draw.draw()
